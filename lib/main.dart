321937bdce3b8d0a3cbe2237d2b57adf5c44cd2f
clshu <clshu2020@gmail.com>
2021-06-08 16:22:48 -0700

    1. Add Scaffold and an AppBar with Title in it.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
-// I need to import a helper lib
-// from flutter to get contents on the screen
+
 import 'package:flutter/material.dart';
 
-// Define a 'main' function to run when our app starts
 void main() {
-// Create a new text widget to show some text on the screen
+
   var app = MaterialApp(
-    home: Text('Hi There'),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Let\'s see some images'),
+        ),
+      ),
   );
-// Take that widget and get it on the screen
 
   runApp(app);
 }
