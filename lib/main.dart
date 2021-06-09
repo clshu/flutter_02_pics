c1ef3e1505954da25e5e3b7b4464e84cd89189c2
clshu <clshu2020@gmail.com>
2021-06-08 17:09:14 -0700

    1. Add a FloadtinActionButton to the Scaffold. 2. Add required onPressed property to the button and a void handler. 3. Add child widget Plus Icon to the button.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
-
 import 'package:flutter/material.dart';
 
 void main() {
-
   var app = MaterialApp(
     home: Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          print('Hi, there');
+        },
+      ),
       appBar: AppBar(
         title: Text('Let\'s see some images'),
-        ),
       ),
+    ),
   );
 
   runApp(app);
