2f8eff04a814ebdd2ebd0c8c871cc32cd14fea0a
clshu <clshu2020@gmail.com>
2021-06-08 21:12:37 -0700

    1. Create ImageList class to render a list of images. 2. Assign ImagList to the body of Scaffold. 3. Set final to serveral variables. 4. ImageList renders the URL list first, then images.

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' show get;
 import 'models/image_model.dart';
 import "dart:convert";
+import "widgets/images_list.dart";
 
 class App extends StatefulWidget {
   @override
@@ -18,10 +19,11 @@ class AppState extends State<App> {
     counter++;
     // This part is slightly different from Stephen's instruction
     // http.get signature is changed
-    var url = Uri.parse('https://jsonplaceholder.typicode.com/photos/$counter');
-    var response = await get(url);
+    final url =
+        Uri.parse('https://jsonplaceholder.typicode.com/photos/$counter');
+    final response = await get(url);
 
-    var imageModel = ImageModel.fromJson(jsonDecode(response.body));
+    final imageModel = ImageModel.fromJson(jsonDecode(response.body));
 
     setState(() {
       images.add(imageModel);
@@ -31,7 +33,7 @@ class AppState extends State<App> {
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
-        body: Text('$counter'),
+        body: ImageList(images),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
           onPressed: fetchImage,
